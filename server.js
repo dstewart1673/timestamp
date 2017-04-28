57b529398ff21cf597f89d722f925eaad4dc7fc5
DStewart1673 <DStewart1673@gmail.com>
2017-04-27 18:46:05 -0700

    cleaning up

--- server.js
+++ server.js
@@ -2,9 +2,6 @@ const express = require('express');
 const port = process.env.PORT || 8080;
 const app = express();
 
-app.get('/', (req, res) => {
-  
-})
 app.get('/:time', (req, res) => {
   const timestamp = new Date(parseInt(req.params.time));
   const data = {};
